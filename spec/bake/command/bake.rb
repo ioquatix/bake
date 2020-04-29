41bfded7dadc20a8648170189489becb77bc4db0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-04-30 01:43:06 +1200

    Add spec for `Bake::Command::Call`.

--- spec/bake/command/bake.rb
+++ spec/bake/command/bake.rb
@@ -1,10 +1,19 @@
 
+def initialize(*)
+	super
+	@called = false
+end
+
+attr :called
+
 # A test method.
 #
 # A second paragraph.
 #
 #
 def test
+	@called = true
+	puts "test method called"
 end
 
 def a_very_unique_method
