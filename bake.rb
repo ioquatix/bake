80e9e17f74bfad406bfc341c716b20924ebd619b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-06-07 14:13:51 +1200

    Show how to provide multiple values to an array argument.

--- bake.rb
+++ bake.rb
@@ -4,3 +4,14 @@
 def print_glob(paths:)
 	puts paths.inspect
 end
+
+# You can invoke this command in one of serveral ways:
+# bake multiple --tags tag1 tag2 tag3 --name "my name"
+# bake multiple --name "my name" --tags tag1 tag2 tag3 \; print_glob --paths *
+# Use "\;" to terminate the list of items for a specific argument.
+#
+# @parameter tags [Array(String)]
+# @parameter name [String]
+def multiple(tags: [], name:)
+	pp [name, tags]
+end
