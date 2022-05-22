95ea69e33edead4e1af7a06b000fe21231c82d26
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-05-23 10:59:06 +1200

    Add file extension detection.

--- bake/input.rb
+++ bake/input.rb
@@ -4,11 +4,15 @@ FORMATS = {
 	yaml: ->(file){require 'yaml'; YAML.load(file.read)},
 }
 
-# Parse an input file (defaulting to stdin) in the specified format (defaulting to JSON).
+# Parse an input file (defaulting to stdin) in the specified format. The format can be extracted from the file extension if left unspecified.
 # @parameter file [Input] The input file.
 # @parameter format [Symbol] The input format, e.g. json, yaml.
-def input(file: $stdin, format: :json)
-	format_for(format).call(file)
+def input(file: $stdin, format: nil)
+	if format = format_for(file, format)
+		format.call(file)
+	else
+		raise "Unable to determine input format of #{file}!"
+	end
 end
 
 # Parse some input text in the specified format (defaulting to JSON).
@@ -16,11 +20,28 @@ end
 # @parameter format [Symbol] The input format, e.g. json, yaml.
 def parse(text, format: :json)
 	file = StringIO.new(text)
-	format_for(format).call(file)
+	
+	if format = format_for(nil, format)
+		format.call(file)
+	else
+		raise "Unable to determine input format!"
+	end
 end
 
 private
 
-def format_for(name)
+def format_for(file, name)
+	if file.respond_to?(:path) and path = file.path
+		name ||= file_type(path)
+	end
+	
 	FORMATS[name]
+end
+
+def file_type(path)
+	if extension = File.extname(path)
+		extension.sub!(/\A\./, '')
+		return if extension.empty?
+		return extension.to_sym
+	end
 end
--- bake/output.rb
+++ bake/output.rb
@@ -7,11 +7,14 @@ FORMATS = {
 }
 
 # Dump the last result to the specified file (defaulting to stdout) in the specified format (defaulting to Ruby's pretty print).
-# @parameter input [Array(Integer)]
 # @parameter file [Output] The input file.
 # @parameter format [Symbol] The output format.
-def output(input:, file: $stdout, format: :pp)
-	format_for(format).call(file, input)
+def output(input:, file: $stdout, format: nil)
+	if format = format_for(file, format)
+		format.call(file, input)
+	else
+		raise "Unable to determine output format!"
+	end
 	
 	# Allow chaining of output processing:
 	return input
@@ -19,6 +22,21 @@ end
 
 private
 
-def format_for(name)
+def format_for(file, name)
+	if file.respond_to?(:path) and path = file.path
+		name ||= file_type(path)
+	end
+	
+	# Default to pretty print:
+	name ||= :pp
+	
 	FORMATS[name]
+end
+
+def file_type(path)
+	if extension = File.extname(path)
+		extension.sub!(/\A\./, '')
+		return if extension.empty?
+		return extension.to_sym
+	end
 end
